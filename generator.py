531185dda645ae61505103e4adc0879da966ac9c
fannyhasbi <fanny.hasbi@kumparan.com>
2019-10-26 19:03:09 +0700

    main is main

--- generator.py
+++ generator.py
@@ -1,13 +1,21 @@
-import secrets, random, string
+#!/usr/local/bin/python3
+import sys
+import secrets, random, string  
+  
+def main():
+  for i in range(10):
+    filename = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
+    with open(filename, 'wb') as f:
+      result = generate_bin_file()
+      print(result)
+      
+      # f.write(result)
 
 def generate_bin_file():
   r = random.randint(50, 100)
   token = secrets.token_hex(r)
   return bytes.fromhex(token)
-
-for i in range(10):
-  filename = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
-  with open(filename, 'wb') as f:
-    result = generate_bin_file()
-    
-    f.write(result)
+      
+if __name__ == "__main__":
+  main()
+  
