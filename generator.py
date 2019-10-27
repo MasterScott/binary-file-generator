f5d6462adaf9985e7780be779c75521d21db2c66
fannyhasbi <fanny.hasbi@kumparan.com>
2019-10-27 14:10:22 +0700

    max hex for single file

--- generator.py
+++ generator.py
@@ -6,29 +6,34 @@ import argparse
 def main():
   print("Initializing...")
   args = init_args()
-  n = 10
+  
+  file_count = 10
+  max_hex = 100
   
   if args.n:
-    n = args.n
+    file_count = args.n
+  if args.x:
+    max_hex = args.x
   
   print("Generating...")
-  for i in range(n):
+  for i in range(file_count):
     filename = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
     with open(filename, 'wb') as f:
-      result = generate_bin_file()
+      result = generate_bin_file(max_hex)
       
       f.write(result)
   
   print("Complete")
 
-def generate_bin_file():
-  r = random.randint(50, 100)
+def generate_bin_file(max_hex):
+  r = random.randint(50, max_hex)
   token = secrets.token_hex(r)
   return bytes.fromhex(token)
 
 def init_args():
   parser = argparse.ArgumentParser()
   parser.add_argument('-n', type=int, help='define number of files')
+  parser.add_argument('-x', type=int, help='define max hex for single file')
   
   return parser.parse_args()
       
