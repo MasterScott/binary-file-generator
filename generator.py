c26e47b46f01077981351dd05b35972474862f32
fannyhasbi <fanny.hasbi@kumparan.com>
2019-10-27 13:43:46 +0700

    generate with terminal args

--- generator.py
+++ generator.py
@@ -1,20 +1,36 @@
 #!/usr/local/bin/python3
 import sys
 import secrets, random, string  
-  
+import argparse
+
 def main():
-  for i in range(10):
+  print("Initializing...")
+  args = init_args()
+  n = 10
+  
+  if args.n:
+    n = args.n
+  
+  print("Generating...")
+  for i in range(n):
     filename = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
     with open(filename, 'wb') as f:
       result = generate_bin_file()
-      print(result)
       
-      # f.write(result)
+      f.write(result)
+  
+  print("Complete")
 
 def generate_bin_file():
   r = random.randint(50, 100)
   token = secrets.token_hex(r)
   return bytes.fromhex(token)
+
+def init_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-n', type=int, help='define number of files')
+  
+  return parser.parse_args()
       
 if __name__ == "__main__":
   main()
